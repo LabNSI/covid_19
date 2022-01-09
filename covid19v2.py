bc7bf127e4efa83369427d535752e3a3aedd4ae7
Marc Silanus <msilanus@gmail.com>
2022-01-09 12:56:08 +0000

    change url

--- covid19v2.py
+++ covid19v2.py
@@ -41,7 +41,7 @@ if __name__ == '__main__':
     # File name of datas
     file = "time_series_19-covid-Confirmed.csv"
     # where datas are located
-    url = "https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+    url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
 
     if download_file(url,file):
         print(f'Téléchargement du fichier {file} terminé avec succès')
