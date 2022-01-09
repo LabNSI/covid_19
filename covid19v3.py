2dd94e49d4af09c2e606b5e14baf5927ad5d6b9e
Marc Silanus <msilanus@gmail.com>
2022-01-09 12:53:36 +0000

    import covid9vx

--- covid19v3.py
+++ covid19v3.py
@@ -3,6 +3,7 @@
 # Filter by country
 # Author : MS
 # Date : 16/03/2020
+# Update : 09/01/2022
 #
 # Replace the "___" by appropriate code
 #
@@ -10,51 +11,13 @@
 
 import urllib.request as urllib2
 import csv
-
-
-def download_file(url, file_name):
-    '''
-    Download a file from url and copy it localy with file_name as name
-    '''
-    try:
-        # open url passed in argument
-        file = urllib2.urlopen(___)
-        # open file for writing in binary mode
-        # https://docs.python.org/3/library/functions.html#open
-        with open(___, '___') as output:
-            # write in output the file read
-            output.___(file.___())
-        # OK : return true
-        return ___
-    except:
-        # Something is wrong : return false
-        return ___
-
-
-def read_CSV(file):
-    '''
-    Read a csv file and return :
-    - fields name as string in a list
-    - datas dictionary  by countries in a list
-    '''
-    # datas is an empty list
-    datas = []
-    # open the file passed in argument as csvfile
-    # https://docs.python.org/3/library/csv.html#csv.reader
-    with open(file) as csvfile:
-        # Create a dictionnary with all datas
-        reader = csv.DictReader(____)
-        # each row in reader is a dictionary by country
-        for row in reader:
-            # add row in datas list
-            datas.___(___)
-        # return the list of dictionaries by countries
-        return datas
+from covid19v1 import *
+from covid19v2 import *
 
 def data_for_country(data, state = "", country = ""):
     # for each country in data
     for pays in ___:
-        # if 'Province\State' key match with state given in argument
+        # if 'Province/State' key match with state given in argument
         # and if 'Country/Region' key match with country given in argument
         if pays[___] == ___ and pays[___] == ___:
             return pays
